6c40d3a288ac4592945fec02b8c82cf39bc0e746
hysyeah <hysyeah@gmail.com>
2026-02-25 21:46:22 +0800

    cli: upgrade l4-bfl-proxy to v0.3.11 (#2557)

--- cli/pkg/upgrade/1_12_5_20260225.go
+++ cli/pkg/upgrade/1_12_5_20260225.go
@@ -7,19 +7,19 @@ import (
 	"github.com/beclab/Olares/cli/pkg/core/task"
 )
 
-type upgrader_1_12_5_20260122 struct {
+type upgrader_1_12_5_20260225 struct {
 	breakingUpgraderBase
 }
 
-func (u upgrader_1_12_5_20260122) Version() *semver.Version {
-	return semver.MustParse("1.12.5-20260122")
+func (u upgrader_1_12_5_20260225) Version() *semver.Version {
+	return semver.MustParse("1.12.5-20260225")
 }
 
-func (u upgrader_1_12_5_20260122) UpgradeSystemComponents() []task.Interface {
+func (u upgrader_1_12_5_20260225) UpgradeSystemComponents() []task.Interface {
 	pre := []task.Interface{
 		&task.LocalTask{
 			Name:   "UpgradeL4BFLProxy",
-			Action: &upgradeL4BFLProxy{Tag: "v0.3.10"},
+			Action: &upgradeL4BFLProxy{Tag: "v0.3.11"},
 			Retry:  3,
 			Delay:  5 * time.Second,
 		},
@@ -28,5 +28,5 @@ func (u upgrader_1_12_5_20260122) UpgradeSystemComponents() []task.Interface {
 }
 
 func init() {
-	registerDailyUpgrader(upgrader_1_12_5_20260122{})
+	registerDailyUpgrader(upgrader_1_12_5_20260225{})
 }
